6dd92e44fc5f39e109a2f50330e4fa18924c899c
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-01-31 16:43:13 +0700

    tutorial 16

---
+++ routing-demo/src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,10 @@
+const ProductDetailsLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <h2>Inner layout</h2>
+      {children}
+    </>
+  );
+};
+
+export default ProductDetailsLayout;
