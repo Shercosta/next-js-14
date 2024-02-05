481aab36c8fac888e33bb46aa843dacae5c5bfa5
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-02-05 21:15:29 +0700

    tutorial 29

--- routing-demo/src/app/complex-dashboard/@notifications/page.tsx
+++ routing-demo/src/app/complex-dashboard/@notifications/page.tsx
@@ -1,6 +1,11 @@
 import Card from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
-  //   return <Card>Notifications</Card>;
-  return <Card children="Notifications in clip" />;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <Link href="/complex-dashboard/archived">Archived</Link>
+    </Card>
+  );
 }
