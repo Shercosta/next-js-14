4b3c9ebf6009eeb8404d78606f080786478f1a6f
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-02-13 21:27:06 +0700

    tutorial 35, yang sebelumnya 34

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,5 +1,21 @@
+import { headers } from "next/headers"
 import { comments } from "./data"
 
 export async function GET() {
     return Response.json(comments)
+}
+
+export async function POST(req: Request) {
+    const comment = await req.json()
+    const newcomment = {
+        id: comments.length + 1,
+        text: comment.text,
+    }
+
+    comments.push(newcomment)
+    return new Response(JSON.stringify(newcomment), {
+        headers: {
+            "Content-Type": "application/json",
+        }, status: 201
+    })
 }
